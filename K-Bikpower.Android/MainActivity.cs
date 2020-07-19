01460c3c8eef4d7ea1fde9141f414b8208182764
Brenton Reddish <brentonr13@gmail.com>
2020-07-19 22:54:50 +1000

    Added Splashscreen, To fix looks will need to change image.

--- K-Bikpower.Android/MainActivity.cs
+++ K-Bikpower.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace K_Bikpower.Droid
 {
-    [Activity(Label = "K_Bikpower", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "K_Bikpower", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
