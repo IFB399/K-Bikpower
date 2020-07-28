1e832de59c334873e57db906818fdc1c22651bc6
Brenton Reddish <brentonr13@gmail.com>
2020-07-28 17:56:08 +1000

    added a hole lot of pages.

---
+++ K-Bikpower/Preview_Asset.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace K_Bikpower
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Preview_Asset : ContentPage
+    {
+        Assets assetdata;
+        public Preview_Asset(Assets details)
+        {
+            InitializeComponent();
+            InitializeComponent();
+            if (details != null)
+            {
+                assetdata = details;
+                PopulateDetails(assetdata);
+            }
+        }
+        private void PopulateDetails(Assets assetdata)
+        {
+            //needs code
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Add_Asset(assetdata));
+        }
+    }
+}
--- K-Bikpower/View_Assets.xaml.cs
+++ K-Bikpower/View_Assets.xaml.cs
@@ -10,11 +10,28 @@ using Xamarin.Forms.Xaml;
 namespace K_Bikpower
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class View_Assets : ContentPage
-{
-    public View_Assets()
+    public partial class View_Assets : ContentPage
     {
-        InitializeComponent();
+        public View_Assets()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            StudentsTable.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Add_Asset(null));
+        }
+        private void EditAsset(object sender, ItemTappedEventArgs e)
+        {
+            Assets details = e.Item as Assets;
+            if (details != null)
+            {
+                Navigation.PushAsync(new Preview_Asset(details));
+            }
+        }
     }
-}
 }
