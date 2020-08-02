4582b872411228698d0c837090ffbc1fc16b3010
Brenton Reddish <brentonr13@gmail.com>
2020-08-02 16:06:50 +1000

    Added New Page

---
+++ K-Bikpower/XAML/Substations/Add_Sub.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace K_Bikpower.XAML.Substations
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class Add_Sub : ContentPage
+{
+    public Add_Sub()
+    {
+        InitializeComponent();
+    }
+}
+}
