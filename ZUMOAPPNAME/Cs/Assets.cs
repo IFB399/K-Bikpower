885b40c3e44507829831c154f74350a730eddb4e
Brenton Reddish <brentonr13@gmail.com>
2020-08-15 20:55:22 +1000

    All of it

---
+++ ZUMOAPPNAME/Cs/Assets.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace K_Bikpower
+{
+    public class Assets
+    {
+        string id;
+        string substation_code;
+        string plant_Number;
+        int asset_EQ_NO;
+        string eQ_status;
+        string serial_number;
+        string modifier_code;
+        int location_equipment_number;
+        string component_code;
+        string warrantydate;
+        int equipement_age;
+        string stock_code;
+        string pO_nO;
+        int rated_Voltage;
+        int nominal_Voltage;
+        string manufacture_Name;
+        string specifiaction_title;
+        string specifiaction_NO;
+        string specifiaction_item_NO;
+        string Last_install_date;
+        string equipment_class;
+        string equipment_class_description;
+        bool done;
+        string name;
+
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [JsonProperty(PropertyName = "substation_code")]
+        public string Substation_Code
+        {
+            get { return substation_code; }
+            set { substation_code = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "plant_number")]
+        public string Plant_Number
+        {
+            get { return plant_Number; }
+            set { plant_Number = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "asset_eq_no")]
+        public int Asset_EQ_NO
+        {
+            get { return asset_EQ_NO; }
+            set { asset_EQ_NO = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "eq_status")]
+        public string EQ_Status
+        {
+            get { return eQ_status; }
+            set { eQ_status = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "serial_number")]
+        public string Serial_Number
+        {
+            get { return serial_number; }
+            set { serial_number = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "modifier_code")]
+        public string Modifier_code
+        {
+            get { return modifier_code; }
+            set { modifier_code = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "location_equipment_number")]
+        public int Location_Equipment_Number
+        {
+            get { return location_equipment_number; }
+            set { location_equipment_number = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "component_code")]
+        public string Component_Code
+        {
+            get { return component_code; }
+            set { component_code = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "warrantydate")]
+        public string WarrantyDate
+        {
+            get { return warrantydate; }
+            set { warrantydate = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "equipment_age")]
+        public int Equipement_age
+        {
+            get { return equipement_age; }
+            set { equipement_age = value; }
+        }
+
+
+        [JsonProperty(PropertyName = "stock_code")]
+        public string Stock_Code
+        {
+            get { return stock_code; }
+            set { stock_code = value; }
+        }
+
+        [JsonProperty(PropertyName = "po_no")]
+        public string PO_NO
+        {
+            get { return pO_nO; }
+            set { pO_nO = value; }
+        }
+
+        [JsonProperty(PropertyName = "rated_volts")]
+        public int Rated_Voltage
+        {
+            get { return rated_Voltage; }
+            set { rated_Voltage = value; }
+        }
+
+        [JsonProperty(PropertyName = "nominal_volts")]
+        public int Nominal_Voltage
+        {
+            get { return nominal_Voltage; }
+            set { nominal_Voltage = value; }
+        }
+
+        [JsonProperty(PropertyName = "manufacture_name")]
+        public string Manufacture_Name
+        {
+            get { return manufacture_Name; }
+            set { manufacture_Name = value; }
+        }
+
+        [JsonProperty(PropertyName = "specifiaction_title")]
+        public string Specifiaction_title
+        {
+            get { return specifiaction_title; }
+            set { specifiaction_title = value; }
+        }
+
+        [JsonProperty(PropertyName = "specification_no")]
+        public string Specifiaction_NO
+        {
+            get { return specifiaction_NO; }
+            set { specifiaction_NO = value; }
+        }
+
+        [JsonProperty(PropertyName = "specifiaction_item_no")]
+        public string Specifiaction_item_NO
+        {
+            get { return specifiaction_item_NO; }
+            set { specifiaction_item_NO = value; }
+        }
+
+        [JsonProperty(PropertyName = "last_install_date")]
+        public string last_install_date
+        {
+            get { return Last_install_date; }
+            set { Last_install_date = value; }
+        }
+
+        [JsonProperty(PropertyName = "equipment_class")]
+        public string Equipment_class
+        {
+            get { return equipment_class; }
+            set { equipment_class = value; }
+        }
+
+        [JsonProperty(PropertyName = "Equimpent_class_decription")]
+        public string Equipment_class_description
+        {
+            get { return equipment_class_description; }
+            set { equipment_class_description = value; }
+        }
+
+        [JsonProperty(PropertyName = "complete")]
+        public bool Done
+        {
+            get { return done; }
+            set { done = value; }
+        }
+        [Version]
+        public string Version { get; set; }
+    }
+}
+
---
+++ ZUMOAPPNAME/XAML/Assets/Add_Asset.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace K_Bikpower
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
+    public partial class Add_Asset : ContentPage
+    {
+        //TableManagerSub subs;
+        Asset_Table_Manager manager;
+        Assets AssetData;
+        string Ids;
+        public Add_Asset(Assets data)
+        {
+            InitializeComponent();
+            manager = Asset_Table_Manager.DefaultManager;
+            //subs = TableManagerSub.DefaultManager;
+            if (data != null)
+            {
+                AssetData = data;
+                PopulateDetails(AssetData);
+                Ids = data.Id;
+                
+            }
+        }
+
+       // protected override async void OnAppearing()
+       // {
+            //base.OnAppearing();
+            //SubPicker.ItemsSource = await App.Database.GetSubAsync();
+            //SubPicker.ItemsSource = await subs.GetTodoItemsAsync(true); //might work dont know
+
+      //  }
+
+        async Task AddItem(Assets item)
+        {
+            await manager.SaveTaskAsync(item);
+        }
+
+        private void PopulateDetails(Assets data)
+        {
+            
+            SubPicker.SelectedItem = data;//check 
+            Plant_Number_Entry.Text = data.Plant_Number;
+            int AssentEQNO = data.Asset_EQ_NO;
+            Asset_EQ_NO_Entry.Text = AssentEQNO.ToString();
+            EQ_Status_Entry.Text = data.EQ_Status;
+            Serial_Number_Entry.Text = data.Serial_Number;
+            Modifier_code_Entry.Text = data.Modifier_code;
+            int Loceqnum = data.Location_Equipment_Number;
+            Location_Equipment_Number_Entry.Text = Loceqnum.ToString();
+            Component_Code_Entry.Text = data.Component_Code;
+            //WarrantyDate_Picker.Date = data.WarrantyDate; //not gonna work
+            int EQUage = data.Equipement_age;
+            Equipement_age_Entry.Text = EQUage.ToString();
+            Stock_Code_Entry.Text = data.Stock_Code;
+            PO_NO_Entry.Text = data.PO_NO;
+            int RatedVolts = data.Rated_Voltage;
+            Rated_Voltage_Entry.Text = RatedVolts.ToString();
+            int NomVolts = data.Nominal_Voltage;
+            Nominal_Voltage_Entry.Text = NomVolts.ToString();
+            Manufacture_Name_Entry.Text = data.Manufacture_Name;
+            Specifiaction_title_Entry.Text = data.Specifiaction_title;
+            Specifiaction_NO_Entry.Text = data.Specifiaction_NO;
+            Specifiaction_item_NO_Entry.Text = data.Specifiaction_item_NO;
+            last_install_date_Entry.Text = data.last_install_date;
+            Equipment_class_Entry.Text = data.Equipment_class;
+            Equipment_class_description_Entry.Text = data.Equipment_class_description;
+            addassetbutton.Text = "Update";
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            //if (!string.IsNullOrWhiteSpace(entry.Text) && !string.IsNullOrWhiteSpace(entry.Text))
+            // {
+            if (addassetbutton.Text == "Add Asset")
+            {
+                var  asset = new Assets
+                {
+                    //Substation_Code = SubPicker.Items[SubPicker.SelectedIndex],
+                    Plant_Number = Plant_Number_Entry.Text,
+                    Asset_EQ_NO = Int32.Parse(Asset_EQ_NO_Entry.Text),
+                    EQ_Status = EQ_Status_Entry.Text,
+                    Serial_Number = Serial_Number_Entry.Text,
+                    Modifier_code = Modifier_code_Entry.Text,
+                    Location_Equipment_Number = Int32.Parse(Location_Equipment_Number_Entry.Text),
+                    Component_Code = Component_Code_Entry.Text,
+                    //WarrantyDate = WarrantyDate_Picker.Date, // change dumb dumb. 
+                    Equipement_age = Int32.Parse(Equipement_age_Entry.Text),
+                    Stock_Code = Stock_Code_Entry.Text,
+                    PO_NO = PO_NO_Entry.Text,
+                    Rated_Voltage = Int32.Parse(Rated_Voltage_Entry.Text),
+                    Nominal_Voltage = Int32.Parse(Nominal_Voltage_Entry.Text),
+                    Manufacture_Name = Manufacture_Name_Entry.Text,
+                    Specifiaction_title = Specifiaction_title_Entry.Text,
+                    Specifiaction_NO = Specifiaction_NO_Entry.Text,
+                    Specifiaction_item_NO = Specifiaction_item_NO_Entry.Text,
+                    last_install_date = last_install_date_Entry.Text,
+                    Equipment_class = Equipment_class_Entry.Text,
+                    Equipment_class_description = Equipment_class_description_Entry.Text,
+                };
+                await AddItem(asset);
+
+            }
+            else
+            {
+                var asset = new Assets
+                {
+
+                    Id = Ids,
+                    Substation_Code = SubPicker.Items[SubPicker.SelectedIndex],
+                    Plant_Number = Plant_Number_Entry.Text,
+                    Asset_EQ_NO = Int32.Parse(Asset_EQ_NO_Entry.Text),
+                    EQ_Status = EQ_Status_Entry.Text,
+                    Serial_Number = Serial_Number_Entry.Text,
+                    Modifier_code = Modifier_code_Entry.Text,
+                    Location_Equipment_Number = Int32.Parse(Location_Equipment_Number_Entry.Text),
+                    Component_Code = Component_Code_Entry.Text,
+                   // WarrantyDate = WarrantyDate_Picker.Date, // change dumb dumb. 
+                    Equipement_age = Int32.Parse(Equipement_age_Entry.Text),
+                    Stock_Code = Stock_Code_Entry.Text,
+                    PO_NO = PO_NO_Entry.Text,
+                    Rated_Voltage = Int32.Parse(Rated_Voltage_Entry.Text),
+                    Nominal_Voltage = Int32.Parse(Nominal_Voltage_Entry.Text),
+                    Manufacture_Name = Manufacture_Name_Entry.Text,
+                    Specifiaction_title = Specifiaction_title_Entry.Text,
+                    Specifiaction_NO = Specifiaction_NO_Entry.Text,
+                    Specifiaction_item_NO = Specifiaction_item_NO_Entry.Text,
+                    last_install_date = last_install_date_Entry.Text,
+                    Equipment_class = Equipment_class_Entry.Text,
+                    Equipment_class_description = Equipment_class_description_Entry.Text,
+                };
+                await AddItem(asset);
+            }
+                // }
+                await Navigation.PushAsync(new TodoList());
+        }
+        //else { await DisplayAlert("Incorrect", "Incorrect username or password", "Close");}
+    }
+}
