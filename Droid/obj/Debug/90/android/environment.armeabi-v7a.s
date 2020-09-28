affc4a6e6a53a92d16e2a50de37f203a7a078e16
Brenton Reddish <brentonr13@gmail.com>
2020-09-28 19:28:24 +1000

    commt

--- Droid/obj/Debug/90/android/environment.armeabi-v7a.s
+++ Droid/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"57a191dc-1f35-487c-9d2d-0d0d2c0d49d9"
+	.asciz	"44acc806-c762-43ab-a223-5665a5300ad4"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
