2669afcdee29611b270aa1dcdbeab0c488c7dfc2
Brenton Reddish <brentonr13@gmail.com>
2020-10-05 14:50:04 +1000

    turn on offline

--- Droid/obj/Debug/90/android/environment.x86.s
+++ Droid/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"dfd92d70-7949-4bb8-97db-ff1dd3930b17"
+	.asciz	"54558576-2185-445c-baf4-fb3e75606226"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
